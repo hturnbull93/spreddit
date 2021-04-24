9c78645b13bdfce57a5da102eeac55ae6b56accb
Harry Turnbull <hturnbull@live.co.uk>
2021-04-24 20:15:04 +0100

    Add SESSION_SECRET to constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,4 @@ dotenv.config();
 export const __prod__ = process.env.NODE_ENV === "production";
 export const DB_USER = process.env.DB_USER;
 export const DB_PASS = process.env.DB_PASS;
+export const SESSION_SECRET = process.env.SESSION_SECRET || "secret";
