37298834c3ed7bad8211073c981f414eb3c9fdb4
Harry Turnbull <hturnbull@live.co.uk>
2021-05-06 20:45:00 +0100

    Add link to CreatePost page on index

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,16 +1,20 @@
 import { withUrqlClient } from "next-urql";
-import NavBar from "../components/NavBar";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
+import Layout from "../components/Layout";
+import NextLink from "next/link";
+import { Link } from "@chakra-ui/layout";
 
 const Index = () => {
   const [{ fetching, data }] = usePostsQuery();
   return (
-    <>
-      <NavBar />
+    <Layout>
+      <NextLink href="/create-post">
+        <Link>Create post</Link>
+      </NextLink>
       {fetching && <div>loading...</div>}
       {data && data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
-    </>
+    </Layout>
   );
 };
 
