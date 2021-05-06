74932352feb2968b03e2d2b40c729bc05e5435b5
Harry Turnbull <hturnbull@live.co.uk>
2021-05-06 20:35:34 +0100

    Add textArea prop to InputField

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -6,22 +6,28 @@ import {
   FormLabel,
 } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
+import { Textarea } from "@chakra-ui/textarea";
 
-type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+type InputFieldProps = InputHTMLAttributes<
+  HTMLInputElement & HTMLTextAreaElement
+> & {
   label: string;
   name: string;
+  textArea?: boolean;
 };
 
 const InputField: React.FC<InputFieldProps> = ({
   label,
   size: _size,
+  textArea,
   ...props
 }) => {
+  let InputOrTextarea = textArea ? Textarea : Input;
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      <InputOrTextarea {...field} {...props} id={field.name} />
       {error && <FormErrorMessage>{error}</FormErrorMessage>}
     </FormControl>
   );
